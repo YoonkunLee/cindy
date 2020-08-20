a8b89935d0da64f3d3038db9bc88b703dbb6ccba
YoonkunLee <ydx9474@aut.ac.nz>
2020-08-20 15:12:11 +1200

    Change MainComponent style

--- src/Components/MainPortfolio.js
+++ src/Components/MainPortfolio.js
@@ -4,24 +4,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Thumbnail1 from '../images/MainPage/Thumbnail1.jpg'
 import Thumbnail2 from '../images/MainPage/Thumbnail2.jpg'
-import Thumbnail3 from '../images/MainPage/Thumbnail3.jpg'
 import Thumbnail4 from '../images/MainPage/Thumbnail4.jpg'
 import Thumbnail5 from '../images/MainPage/Thumbnail5.jpg'
-import project1 from '../images/Project 1/Thumbnail.jpg'
+import Thumbnail6 from '../images/MainPage/BrandGuideline.jpg'
+import Thumbnail7 from '../images/MainPage/Creambly.jpg'
 import '../CSS/MainPortfolio.css'
 
 export default class Portfolio extends Component {
     render(){
         return (
             <div class="container-fluid">
-                <h1>Portfolio</h1>
+                <h1 style={PortfolioText}>PORTFOLIO</h1>
                 <div class="row">
                     <div class="grid">
                         <figure class="effect-lily">
-                            <img src={Thumbnail1} alt="img12"/>
+                            <img src={Thumbnail7} alt="img12"/>
                             <figcaption>
                                 <div>
-                                    <h2>LucidStand</h2>
+                                    <h2>Creambly</h2>
                                 </div>
                                 <Link to={"../PortfolioDetail/" + 0} activeClassName={"active"}></Link>
                             </figcaption>			
@@ -36,37 +36,37 @@ export default class Portfolio extends Component {
                             </figcaption>			
                         </figure>
                         <figure class="effect-lily">
-                            <img src={Thumbnail1} alt="img12"/>
+                            <img src={Thumbnail2} alt="img12"/>
                             <figcaption>
                                 <div>
-                                    <h2>LucidStand</h2>
+                                    <h2>IPA</h2>
                                 </div>
                                 <a href="../PortfolioDetail/LucidStand"></a>
                             </figcaption>			
                         </figure>
                         <figure class="effect-lily">
-                            <img src={Thumbnail1} alt="img12"/>
+                            <img src={Thumbnail4} alt="img12"/>
                             <figcaption>
                                 <div>
-                                    <h2>LucidStand</h2>
+                                    <h2>Anual Report</h2>
                                 </div>
                                 <a href="../PortfolioDetail/LucidStand"></a>
                             </figcaption>			
                         </figure>
                         <figure class="effect-lily">
-                            <img src={Thumbnail1} alt="img12"/>
+                            <img src={Thumbnail5} alt="img12"/>
                             <figcaption>
                                 <div>
-                                    <h2>LucidStand</h2>
+                                    <h2>Conferenz EDM</h2>
                                 </div>
                                 <a href="../PortfolioDetail/LucidStand"></a>
                             </figcaption>			
                         </figure>
                         <figure class="effect-lily">
-                            <img src={Thumbnail1} alt="img12"/>
+                            <img src={Thumbnail6} alt="img12"/>
                             <figcaption>
                                 <div>
-                                    <h2>LucidStand</h2>
+                                    <h2>Brand Guideline</h2>
                                 </div>
                                 <a href="../PortfolioDetail/LucidStand"></a>
                             </figcaption>			
@@ -78,36 +78,8 @@ export default class Portfolio extends Component {
     }
 }
 
-const ImageToggleOnMouseOver = ({primaryImg, secondaryImg}) => {
-    const imageRef = useRef(null);
-  
-    return (
-      <img  class="col-md-6 pl-0 pr-0 float-left"
-        onMouseOver={() => {
-          imageRef.current.src = secondaryImg;
-        }}
-        onMouseOut={() => {
-          imageRef.current.src= primaryImg;
-        }}
-        src={primaryImg} 
-        alt=""
-        ref={imageRef}
-      />
-    )
-  }
-
-  const ImageChangeOnMouseOver = () => {
-    return (
-      <div class="col-md-12 pl-0 pr-0 overflow-hidden float-left ">
-        <ImageToggleOnMouseOver 
-          primaryImg={Thumbnail1}
-          secondaryImg={Thumbnail2}
-          alt="" />
-        <ImageToggleOnMouseOver
-          primaryImg={Thumbnail4}
-          secondaryImg={Thumbnail5}
-          alt="" />
-      </div>
-    )
-  }
-  
+var PortfolioText = {
+    marginTop: "150px",
+    textAlign: "center",
+    marginBottom: "50px"
+}
--- src/Components/NavMenu.js
+++ src/Components/NavMenu.js
@@ -3,13 +3,15 @@ import {Link} from 'react-router-dom';
 import '../CSS/NavMenu.css';
 import logo from '../images/MainPage/logo.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo1 from '../images/MainPage/CJ final.png';
+import logo2 from '../images/MainPage/CJ creative.png';
 
 export default class NavMenu extends Component {
     render() {
       return (
         <div className="container-fluid ">       
-            <div className="col-3 float-left">
-                <Link to="/"><img src={logo} width="75" height="40" alt="Logo" /></Link> 
+            <div className="col-3 float-left overflow-hidden mt-3">
+                <Link to="/"><img src={logo1} width="94" height="67" alt="Logo" /></Link> 
             </div>                 
             <nav className="navbar navbar-light cl-effect-1">          
                 <a href="/#portfolio" style={navText}>Portfolio</a>
