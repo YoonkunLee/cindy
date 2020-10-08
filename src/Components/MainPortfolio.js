b654bd2d4efaddf53d0a50e2fed5100119edee1c
YoonkunLee <ydx9474@aut.ac.nz>
2020-10-09 09:48:37 +1300

    Signed-off-by: YoonkunLee <ydx9474@aut.ac.nz>

--- src/Components/MainPortfolio.js
+++ src/Components/MainPortfolio.js
@@ -28,10 +28,7 @@ export default class Portfolio extends Component {
           document.body.style.overflow = "hidden";
           document.documentElement.style.overflow = "hidden";
       }
-
-      
-
-
+ 
     render(){
         return (
             <div className="body">
@@ -59,20 +56,18 @@ export default class Portfolio extends Component {
                                         </div>                                                       
                                         <div className="list">                                                                                
                                             {PortfolioData[this.state.id].images.map((image) =>{  
-                                                if(image === "/images/Genless/Genless3.jpg"){
-                                                    return <FadeInSection> 
-                                                        <div className="popuptextmargin textStyleBody">I believe creating a storyline was the key factor of this brief. My solution of how to use this 3-5frames to market the buyers guide was to have a simple Q&A. I read through the guide thoroughly and picked 2 useful info that I believe are one of the common questions or thoughts that most people would have, especially the ones who never experienced EV before. So the first and the third frame throw questions and the second and the fourth frame answers to it. The last frame as you can see below is the call to action message condensing the fact that there is an EV guide available with more details online.</div>
-                                                        <img alt={image.alt} width="100%" effect="opacity" key={image.key} src={ process.env.PUBLIC_URL + "/images/Genless/Genless3.jpg"}></img>
-                                                    </FadeInSection>                                                    
-                                                }   
-                                                else if(image === "/images/Genless/Genless5.jpg"){
+                                                if(image.Path === "/images/Genless/Genless6.jpg"){
                                                     return <FadeInSection>
-                                                        <img alt={image.alt} width="100%" effect="opacity" key={image.key} src={ process.env.PUBLIC_URL + "/images/Genless/Genless5.jpg"}></img>
+                                                        <img alt={image.alt} width="100%" effect="opacity" key={image.key} src={ process.env.PUBLIC_URL + "/images/Genless/Genless6.jpg"}></img>
                                                         <video autoPlay loop controls className="maxwidth" src={process.env.PUBLIC_URL + PortfolioData[this.state.id].video}></video>       
                                                     </FadeInSection>                                                                            
                                                 }
                                                 else{
-                                                    return <FadeInSection key={image}><img className="marginbottom" alt={image.alt} width="100%" effect="opacity" key={image.key} src={ process.env.PUBLIC_URL + image}></img>
+                                                    return <FadeInSection key={image}>
+                                                        <div>
+                                                            <img className="marginbottom" alt={image.alt} width="100%" effect="opacity" key={image.key} src={ process.env.PUBLIC_URL + image.Path}></img>  
+                                                            <div className="popuptextmargin textStyleBody">{image.imageText}</div>                                                          
+                                                        </div>                                                        
                                                     </FadeInSection>
                                                     
                                                 }                                                                                  
